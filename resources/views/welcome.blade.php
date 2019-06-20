b6a9898a1917c950142384540c2537ff2db0e055
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-06-20 17:58:42 +0530

    init

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,13 +29,13 @@
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
                     data-target="#bs-example-navbar-collapse-1"><span class="sr-only">Toggle navigation</span> Menu <i
                         class="fa fa-bars"></i></button>
-                <a class="navbar-brand page-scroll" href="#page-top">Pauzr</a></div>
+                <a class="navbar-brand page-scroll" href="#page-top">PauzR</a></div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li class=""><a class="page-scroll" href="#how-it-works">How it works</a></li>
-                    <li class=""><a class="page-scroll" href="#download">Download</a></li>
-                    <li class=""><a class="page-scroll" href="#features">Features</a></li>
-                    <li class=""><a class="page-scroll" href="#contact">Contact</a></li>
+                    <li class=""><a class="page-scroll" style="color:white" href="#how-it-works">How it works</a></li>
+                    <li class=""><a class="page-scroll" style="color:white" href="#download">Download</a></li>
+                    <li class=""><a class="page-scroll" style="color:white" href="#features">Features</a></li>
+                    <li class=""><a class="page-scroll" style="color:white" href="#contact">Contact</a></li>
                 </ul>
             </div>
         </div>
