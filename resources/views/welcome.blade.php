630359d0b74278ca33b5341bda3815c01b2325c6
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-06-20 17:12:18 +0530

    init

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -94,9 +94,11 @@
                 <div class="col-md-8 col-md-offset-2">
                     <h2 class="section-heading">Discover what all the buzz is about!</h2>
                     <p>Our app is available on any mobile device! Download now to get started!</p>
-                    <div class="badges"><a class="badge-link" href="#"><img src="/images/google-play-badge.svg"
-                                alt=""></a> <a class="badge-link" href="#"><img src="/images/app-store-badge.svg"
-                                alt=""></a>
+                    <div class="badges">
+                        <a class="badge-link" href="#">
+                            <img src="/images/google-play-badge.svg" alt="">
+                        </a>
+                        {{-- <a class="badge-link" href="#"><img src="/images/app-store-badge.svg" alt=""></a> --}}
                     </div>
                 </div>
             </div>
