460db2c30d2509092410f797b2ffdc6c57befea0
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-06-21 15:16:10 +0530

    init

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -133,7 +133,8 @@
                     <h2 class="section-heading">Discover what all the buzz is about!</h2>
                     <p>Our app is available on any android device! Download now to get started!</p>
                     <div class="badges">
-                        <a class="badge-link" href="https://play.google.com/store/apps/details?id=com.pauzr.org">
+                        <a class="badge-link" target="_blank"
+                            href="https://play.google.com/store/apps/details?id=com.pauzr.org">
                             <img src="/images/google-play-badge.svg" alt="">
                         </a>
                         {{-- <a class="badge-link" href="#"><img src="/images/app-store-badge.svg" alt=""></a> --}}
